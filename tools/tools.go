3ece1448e1e5a8c47ca8e6805001fba67811ef65
Pavel Perminov <start@perminov.im>
2024-09-25 22:49:15 +0300

    Now unpacking

--- tools/tools.go
+++ tools/tools.go
@@ -1,6 +1,15 @@
 package tools
 
 import (
+	"archive/tar"
+	"archive/zip"
+	"compress/bzip2"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/davecgh/go-spew/spew"
@@ -26,4 +35,122 @@ func ParseTime(t string) time.Time {
 
 func Dumper(v any) {
 	spew.Dump(v)
+}
+
+func UnpackArchive(src, dest string) error {
+	switch {
+	case strings.HasSuffix(src, ".zip"):
+		return unzip(src, dest)
+	case strings.HasSuffix(src, ".tar"):
+		return untar(src, dest, "")
+	case strings.HasSuffix(src, ".tar.gz"), strings.HasSuffix(src, ".tgz"):
+		return untar(src, dest, "gzip")
+	case strings.HasSuffix(src, ".tar.bz2"):
+		return untar(src, dest, "bzip")
+	default:
+		return fmt.Errorf("unsupported file format")
+	}
+}
+
+func unzip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, file := range r.File {
+		fpath := filepath.Join(dest, file.Name)
+
+		if !filepath.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", fpath)
+		}
+
+		if file.FileInfo().IsDir() {
+			os.MkdirAll(fpath, os.ModePerm)
+			continue
+		}
+
+		if err := os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+
+		rc, err := file.Open()
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func untar(src, dest, decompressor string) error {
+	file, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var r io.Reader = file
+
+	switch decompressor {
+	case "gzip":
+		r, _ = gzip.NewReader(file)
+	case "bzip":
+		r = bzip2.NewReader(file)
+	}
+
+	tarReader := tar.NewReader(r)
+
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		fpath := filepath.Join(dest, header.Name)
+
+		if !filepath.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+			return fmt.Errorf("illegal file path: %s", fpath)
+		}
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if err := os.MkdirAll(fpath, os.FileMode(header.Mode)); err != nil {
+				return err
+			}
+		case tar.TypeReg:
+			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			if _, err := io.Copy(outFile, tarReader); err != nil {
+				outFile.Close()
+				return err
+			}
+			outFile.Close()
+		default:
+			return fmt.Errorf("unsupported type: %v in %s", header.Typeflag, header.Name)
+		}
+	}
+
+	return nil
 }
