3f042a5449a0d4ce78e7fa9afb92dfe29b68c927
Pavel Perminov <start@perminov.im>
2024-09-25 22:51:34 +0300

    Upload data endpoint implemented

--- myGin/gin.go
+++ myGin/gin.go
@@ -3,7 +3,10 @@ package mygin
 import (
 	"fmt"
 	"math/rand/v2"
+	"net/http"
+	"os"
 	"polar_reflow/hrv"
+	importData "polar_reflow/import"
 	"polar_reflow/tools"
 	"strconv"
 	"time"
@@ -14,7 +17,27 @@ import (
 func Run() {
 	engine := gin.Default()
 	engine.Use(auth)
-	engine.PUT("/upload_data", func(ctx *gin.Context) {})
+	engine.PUT("/uploaddata", func(ctx *gin.Context) {
+		file, err := ctx.FormFile("file")
+		if err != nil {
+			ctx.String(http.StatusBadRequest, "File upload error: %s", err.Error())
+			return
+		}
+		filePath := "/tmp/" + file.Filename
+		if err := ctx.SaveUploadedFile(file, filePath); err != nil {
+			ctx.String(http.StatusInternalServerError, "Unable to save the file: %s", err.Error())
+			return
+		}
+		ctx.String(http.StatusOK, fmt.Sprintf("'%s' uploaded successfully!", file.Filename))
+		if err := tools.UnpackArchive(filePath, "/tmp/"); err != nil {
+			ctx.String(http.StatusInternalServerError, "Unable to unpack zip file: %s", err.Error())
+			return
+		}
+
+		// Optionally, delete the temp file after extraction
+		os.Remove(filePath)
+		importData.ImportFiles("/tmp")
+	})
 	engine.GET("/hrv/last5min", getRealHRVRMSSD)
 	engine.GET("/hrv/5minforperiod", getRealHRVRMSSDMinByMin)
 	engine.GET("/ping", func(ctx *gin.Context) {
@@ -46,7 +69,7 @@ func getRealHRVRMSSDMinByMin(ctx *gin.Context) {
 	tools.ErrPanic(err)
 	to, err := time.Parse(time.RFC3339, params.Get("to"))
 	tools.ErrPanic(err)
-	
+
 	ctx.JSON(200, hrv.Get5MinRMSSDFromtimeToTime(from, to))
 }
 
